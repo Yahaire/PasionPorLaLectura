b82b73d0a0330f4be0e0b00623ca62e445058044
Jesus Alejandro Reyes <jesus.reyes.96@hotmail.com>
2017-09-28 10:13:14 -0600

    added Vuforia

---
+++ PasionXlaLectura/Assets/LoadLevels.js
@@ -0,0 +1,47 @@
+﻿ #pragma strict
+ public var Level = "Avance1";
+ 
+ function LoadLevel()
+ {
+ 	Application.LoadLevel(Level);
+ }
+
+ function Home()
+ {
+ 	Application.LoadLevel("Inicio");
+ }
+
+ function LosCien()
+ {
+ 	Application.LoadLevel("100");
+ }
+
+  function Info()
+ {
+ 	Application.LoadLevel("Info");
+ }
+
+  function Instrucciones()
+ {
+ 	Application.LoadLevel("Instrucciones");
+ }
+
+  function Facebook()
+ {
+ 	Application.OpenURL ("https://www.facebook.com/pasionxlecturamty");
+ }
+
+ function Twitter()
+ {
+ 	Application.OpenURL ("https://twitter.com/PasionxLectura");
+ }
+
+ function URL()
+ {
+ 	Application.OpenURL ("http://www.pasionporlalectura.itesm.mx/");
+ }
+
+ function Salir()
+ {
+ 	Application.Quit();
+ }
