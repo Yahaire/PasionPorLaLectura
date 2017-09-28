ff8b2acbeabe145877789c5e973251b57da81482
CrisTJ <cristinajimenezcanales@gmail.com>
2017-09-28 09:45:40 -0500

    PPLLMenu
    update

---
+++ PasionXlaLectura/Assets/loadscenes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadscenes: MonoBehaviour{
+
+	public void loadExploar(){
+		SceneManager.LoadScene("exploraPPLL");
+	}
+}
